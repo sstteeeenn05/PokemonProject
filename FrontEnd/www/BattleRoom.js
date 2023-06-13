7c7ea44e955969ec23d60ce8e641764536c31781
ASOIR <cn.921221@gmail.com>
2023-06-14 04:31:34 +0800

    Battle Room UI look done
    Function haven't implemented. Only the UI's look  is done

--- FrontEnd/www/BattleRoom.js
+++ FrontEnd/www/BattleRoom.js
@@ -1,5 +1,58 @@
 import './jsapi/Alpine.js'
 import {Database} from "./jsapi/Database.js"
-import {XHR} from "./jsapi/XHR.js"
 
-let db=new Database();
+let db = new Database();
+
+document.addEventListener('alpine:init', () => {
+  Alpine.store('modal', {
+    type: '',
+    listData: [1, 2, 3, 4],
+    conIconList: [
+      'BRN.svg',
+      'PSN.svg',
+      'PAR.svg'
+    ],
+    typeIconList: [
+      'Normal.png',
+      'Fire.png',
+      'Water.png',
+      'Electric.png',
+      'Grass.png',
+      'Ice.png',
+      'Fight.png',
+      'Poison.png',
+      'Ground.png',
+      'Flying.png',
+      'Psychic.png',
+      'Bug.png',
+      'Rock.png',
+      'Ghost.png',
+      'Dragon.png',
+      'Dark.png',
+      'Steel.png',
+      'Fairy.png'
+    ],
+    potionList: [
+      ['potion.png', 'Potion', 'Heal 20 Hp of your pokémon'],
+      ['superPotion.png', 'Super Potion', 'Heal 60 Hp of your pokémon'],
+      ['hyperPotion.png', 'Hyper Potion', 'Heal 120 Hp of your pokémon'],
+      ['maxPotion.png', 'Max Potion', 'Heal 100% Hp of your pokémon']
+    ],
+    pokemonType: [false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,],
+    pokemonCondition: [false, true, true],
+    skillType: [true, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false,],
+    skillCondition: [false, true, false],
+    selectingPokemon() {
+      this.type = 'pokemon';
+    },
+    open(type) {
+      this.type = type;
+      let modal = document.getElementById('list')
+      modal.showModal()
+    },
+    close() {
+      let modal = document.getElementById('list')
+      modal.close();
+    }
+  })
+})
